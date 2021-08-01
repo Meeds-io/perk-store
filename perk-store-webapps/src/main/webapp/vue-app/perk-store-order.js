b80aa531dbcbad424413c0f3ed5ac25ec130249d
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-08-01 15:14:24 +0100

    TASK-47234 Defer loading of cacheable widgets to load principal application in page first

--- perk-store-webapps/src/main/webapp/vue-app/perk-store-order.js
+++ perk-store-webapps/src/main/webapp/vue-app/perk-store-order.js
@@ -14,7 +14,9 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import './initComponents.js';
+import PerkstoreOrderApp from './components/perk-store-order/PerkstoreOrderApp.vue';
+
+Vue.component('perk-store-order-app', PerkstoreOrderApp);
 
 Vue.use(Vuetify);
 
