fef38ddbd69772d9aeb16b29e3366c2a20bfd68a
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-07-18 17:38:13 +0100

    MEED-345 : Refund option with Metamask account in a drawer .  (#202)
    prior to this change, the refunding process was experienced throughout a modal and was only tolerant to the internal wallet, this change has enabled metamask refunding option throughout exo-drawer with alerting system .

--- perk-store-webapps/src/main/webapp/vue-app/components/perk-store/OrderDetail.vue
+++ perk-store-webapps/src/main/webapp/vue-app/components/perk-store/OrderDetail.vue
@@ -136,7 +136,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
               <button
                 v-if="canRefundOrder"
                 class="ignore-vuetify-classes btn orderProcessingBtn text-truncate text-truncate me-1 ms-1"
-                @click="$refs.refundModal.open()">
+                @click="$refs.refundDrawer.open()">
                 {{ $t('exoplatform.perkstore.button.refund') }}
               </button>
               <perk-store-deliver-modal
@@ -144,15 +144,15 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
                 ref="deliverModal"
                 :product="orderProduct"
                 :order="order" />
-              <perk-store-refund-modal
+              <perk-store-refund-drawer
                 v-if="canRefundOrder"
-                ref="refundModal"
+                ref="refundDrawer"
                 :product="orderProduct"
                 :order="order"
                 :symbol="symbol"
                 @refunding="refunding = true"
                 @refunded="refunded"
-                @closed="refundDialogClosed" />
+                @closed="refundDrawerClosed" />
             </template>
             <div v-else-if="isCanceled">
               {{ $t('exoplatform.perkstore.label.status.canceled') }}
@@ -347,7 +347,7 @@ export default {
     refunded() {
       // Nothing to do, the update will be made by Websocket
     },
-    refundDialogClosed() {
+    refundDrawerClosed() {
       // We have to re-init wallet settings to the current user instead of wallet of Order receiver
       this.$emit('init-wallet');
       this.refunding = false;
