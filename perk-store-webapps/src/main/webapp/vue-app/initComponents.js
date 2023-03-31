c55b813ced6dc4730bb059a0d4ef1a50c23bd9fb
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-03-31 14:09:42 +0100

    feat: Perkstore toolbar enhancement - MEED-2015 - Meeds-io/meeds#865 (#257)
    Put perkstore toolbar in a dedicated component.

--- perk-store-webapps/src/main/webapp/vue-app/initComponents.js
+++ perk-store-webapps/src/main/webapp/vue-app/initComponents.js
@@ -5,7 +5,6 @@ import BuyModal from './components/perk-store/BuyModal.vue';
 import ProductsList from './components/perk-store/ProductsList.vue';
 import ProductDetail from './components/perk-store/ProductDetail.vue';
 import ProductForm from './components/perk-store/ProductForm.vue';
-import CreateProductButton from './components/perk-store/CreateProductButton.vue';
 import DeliverModal from './components/perk-store/DeliverModal.vue';
 import FileMultiUploadInput from './components/perk-store/FileMultiUploadInput.vue';
 import ImageAttachmentSelector from './components/perk-store/ImageAttachmentSelector.vue';
@@ -18,6 +17,7 @@ import ProductNotification from './components/perk-store/ProductNotification.vue
 import PerkStoreProductSearchCard from './components/perk-store-search/PerkStoreProductSearchCard.vue';
 import PerkStoreAlert from './components/perk-store/PerkStoreAlert.vue';
 import RefundDrawer from './components/perk-store/RefundDrawer.vue';
+import PerkStoreToolbar from './components/perk-store/PerkStoreToolbar.vue';
 
 const components = {
   'perk-store-auto-complete': AutoComplete,
@@ -27,7 +27,6 @@ const components = {
   'perk-store-products-list': ProductsList,
   'perk-store-product-detail': ProductDetail,
   'perk-store-product-form': ProductForm,
-  'perk-store-create-product-button': CreateProductButton,
   'perk-store-deliver-modal': DeliverModal,
   'perk-store-upload-input': FileMultiUploadInput,
   'perk-store-image-attachment-selector': ImageAttachmentSelector,
@@ -40,6 +39,7 @@ const components = {
   'perk-store-product-search-card': PerkStoreProductSearchCard,
   'perk-store-alert': PerkStoreAlert,
   'perk-store-refund-drawer': RefundDrawer,
+  'perk-store-toolbar': PerkStoreToolbar,
 };
 
 for (const key in components) {
