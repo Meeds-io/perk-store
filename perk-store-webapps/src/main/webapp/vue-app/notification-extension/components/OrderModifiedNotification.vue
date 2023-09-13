76f1d1b9bb2f7956398f49409ae67cb17bb32c20
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-09-13 19:29:29 +0100

    feat: Enhance Web Notifications UI and UX - MEED-2488 - Meeds-io/MIPs#80 (#268)

---
+++ perk-store-webapps/src/main/webapp/vue-app/notification-extension/components/OrderModifiedNotification.vue
@@ -0,0 +1,63 @@
+<template>
+  <user-notification-template
+    :notification="notification"
+    :avatar-url="profileAvatarUrl"
+    :message="message"
+    :url="orderUrl">
+    <template #actions>
+      <div class="text-truncate">
+        <v-icon size="14" class="me-1">fa-shopping-cart</v-icon>
+        {{ productTitle }}
+      </div>
+    </template>
+  </user-notification-template>
+</template>
+<script>
+export default {
+  props: {
+    notification: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    productTitle() {
+      return this.notification?.parameters?.PRODUCT_TITLE;
+    },
+    productId() {
+      return this.notification?.parameters?.PRODUCT_ID;
+    },
+    orderId() {
+      return this.notification?.parameters?.ORDER_ID;
+    },
+    modifierId() {
+      return this.notification?.parameters?.MODIFIER_ID;
+    },
+    senderId() {
+      return this.notification?.parameters?.SENDER_ID;
+    },
+    isSender() {
+      return this.senderId === eXo.env.portal.userIdentityId;
+    },
+    orderStatus() {
+      return this.notification?.parameters?.ORDER_STATUS?.toLowerCase();
+    },
+    orderUrl() {
+      return `${eXo.env.portal.context}/${eXo.env.portal.portalName}/perkstore?productId=${this.productId}&orderId=${this.orderId}`;
+    },
+    profile() {
+      return this.notification?.from;
+    },
+    profileAvatarUrl() {
+      return this.profile?.avatar;
+    },
+    message() {
+      return this.modifierId && this.$t('Notification.perkstore.order.modified', {
+        0: `<a class="user-name font-weight-bold">${this.profile?.fullname}</a>`,
+      }) || this.$t(`Notification.perkstore.order.modified.short.${this.orderStatus}.system${this.isSender && '.you' || ''}`, {
+        0: `<a class="user-name font-weight-bold">${this.profile?.fullname}</a>`,
+      });
+    },
+  },
+};
+</script>
