e391534014b1f5d8e351eef8a67fc861000915e7
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-09-15 17:07:03 +0100

    feat: Replace Web Notification rendering engine from gtmpl to Vue - MEED-2479 - Meeds-io/MIPs#80 (#270)
    This change will allow to not having a gtmpl template associated for some Web Notifications to use Frontend side display instead. This will ensure a better UX, evolutivity and maintainability.

---
+++ perk-store-webapps/src/main/webapp/vue-app/notification-extension/components/ProductModifiedNotification.vue
@@ -0,0 +1,40 @@
+<template>
+  <user-notification-template
+    :notification="notification"
+    :message="message"
+    :url="productUrl">
+    <template #avatar>
+      <v-icon size="32">fa-store-alt</v-icon>
+    </template>
+    <template #actions>
+      <div class="text-truncate">
+        <v-icon size="14" class="me-1">fa-shopping-cart</v-icon>
+        {{ productTitle }}
+      </div>
+    </template>
+  </user-notification-template>
+</template>
+<script>
+export default {
+  props: {
+    notification: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    productTitle() {
+      return this.notification?.parameters?.PRODUCT_TITLE;
+    },
+    productId() {
+      return this.notification?.parameters?.PRODUCT_ID;
+    },
+    productUrl() {
+      return `${eXo.env.portal.context}/${eXo.env.portal.portalName}/perkstore/catalog/${this.productId}`;
+    },
+    message() {
+      return this.$t('UINotification.label.ProductModifiedNotificationPlugin');
+    },
+  },
+};
+</script>
