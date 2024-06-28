00f583cb1f44d41a44a140acaa4b12ef9f46d1f3
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-06-28 07:49:19 +0100

    feat: Change default font sizes - MEED-7147 - Meeds-io/MIPs#144

--- perk-store-webapps/src/main/webapp/vue-app/perk-store-admin/components/PerkStoreAdminSetup.vue
+++ perk-store-webapps/src/main/webapp/vue-app/perk-store-admin/components/PerkStoreAdminSetup.vue
@@ -22,10 +22,10 @@
 <template>
   <v-app flat>
     <div class="px-6">
-      <v-list-item class="px-0" two-line>
+      <v-list-item class="px-0" two-line dense>
         <v-list-item-content>
-          <v-list-item-title>
-            <h4 class="my-0">{{ $t('perkstore.administration.title') }}</h4>
+          <v-list-item-title class="text-title">
+            {{ $t('perkstore.administration.title') }}
           </v-list-item-title>
           <v-list-item-subtitle>
             {{ $t('perkstore.administration.subtitle') }}
